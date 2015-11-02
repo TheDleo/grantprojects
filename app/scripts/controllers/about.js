4f27acd22127e866e5e0a3b9b6376d11b9839484
Dan Martin <dmartin@fh.org>
2015-11-02 15:13:08 -0700

    initial commit of project files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name grantprojectsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the grantprojectsApp
+ */
+angular.module('grantprojectsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name grantprojectsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the grantprojectsApp
+ */
+angular.module('grantprojectsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
